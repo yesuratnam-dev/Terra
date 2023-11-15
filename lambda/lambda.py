732a5e2f9b9b4c40c0d1a6dae0f325e19d73096a
Yesuratnam Kunchala <kuyesura@amazon.com>
2023-11-15 17:52:58 +0530

    updated

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,10 +1,14 @@
-def hello_lambda(event, context):
-    return {
-        'statusCode': 200,
-        'body': "Hello World!"
-        }
+import boto3
+import pandas as pd
 
-print(hello_lambda("a","b")['statusCode'])
+def hello_lambda(event, context):
+
+    df = pd.read_csv("s3://kuyesura-acer-s3/flights.csv")
+
+    output = df.head()
+
+    output.to_csv("s3://kuyesura-acer-s3/output.csv", index=False)
+
 
 if __name__ == "__main__":
     hello_lambda("a","b")
--- lambda/main.tf
+++ lambda/main.tf
@@ -16,7 +16,11 @@
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   # assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  assume_role_policy = file("policy.json")
+  assume_role_policy = file("lambda/policy.json")
+
+  inline_policy {
+    policy = file("lambda/policy.json")
+  }
 }
 
 data "archive_file" "lambda" {
@@ -25,17 +29,38 @@ data "archive_file" "lambda" {
   output_path = "lambda_function_payload.zip"
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
+
+
+resource "aws_lambda_layer_version" "python_layer" {
+  layer_name = "layer"
+  description = "My Python Lambda Layer"
+
+  compatible_runtimes = ["python3.10"]  # Specify the runtime(s) your layer supports
+
+  s3_bucket = "kuyesura-s3"
+
+  s3_key = "layer.zip"
+
+
+  # Path to the directory containing your Python dependencies
+  # source_code_hash = filebase64sha256("my_lambda_function/boto_lib.zip")
+
+  # Configuration for the AWS Lambda Layer
+  # filename = "my_lambda_function/boto_lib.zip"  # Specify the correct path to your ZIP file
+}
+
+resource "aws_lambda_function" "ETL_lambda" {
+
+  layers = [aws_lambda_layer_version.python_layer.arn]
+
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+
+  function_name = "ETL_Lambda"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler = "lambda.hello_lambda"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "nodejs18.x"
+  runtime = "python3.10"
 
   environment {
     variables = {
